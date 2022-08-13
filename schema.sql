fbd131980d8360f51c46af22cb5d4004d787977b
David Truong <dtruong1992@gmail.com>
2022-08-13 12:32:50 -0700

    ETL process complete

--- schema.sql
+++ schema.sql
@@ -9,28 +9,28 @@ DROP SCHEMA IF EXISTS reviews;
 CREATE SCHEMA reviews;
 
 
-CREATE TABLE reviews.products (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(255),
-  slogan VARCHAR(255),
-  description VARCHAR(255),
-  category VARCHAR(255),
-  default_price VARCHAR(255)
-);
+-- CREATE TABLE reviews.products (
+--   id SERIAL PRIMARY KEY,
+--   name VARCHAR(255),
+--   slogan VARCHAR(255),
+--   description VARCHAR(255),
+--   category VARCHAR(255),
+--   default_price VARCHAR(255)
+-- );
 
 
 CREATE TABLE reviews.reviews (
   id SERIAL PRIMARY KEY,
-  products_id INTEGER REFERENCES reviews.products,
+  product_id INTEGER,
   rating INTEGER DEFAULT 0,
-  date TIMESTAMP,
+  date VARCHAR(100),
   summary VARCHAR(255),
-  body VARCHAR(255),
+  body VARCHAR(1000),
   recommend BOOLEAN,
   reported BOOLEAN DEFAULT FALSE,
-  reviewer_name VARCHAR(255),
-  reviewer_email VARCHAR(255),
-  response VARCHAR(255),
+  reviewer_name VARCHAR(60),
+  reviewer_email VARCHAR(60),
+  response VARCHAR,
   helpfulness INTEGER DEFAULT 0
 );
 
@@ -52,3 +52,8 @@ CREATE TABLE reviews.characteristics (
   length INTEGER,
   fit INTEGER
 );
+
+-- \COPY reviews.reviews (id,product_id,rating,date,summary,body,recommend,reported,reviewer_name,reviewer_email,response,helpfulness)
+-- FROM '/Users/davidtruong/Desktop/Hack Reactor SE/rpp36-david-overview/data/reviews.csv' DELIMITER ',' CSV HEADER;
+
+
